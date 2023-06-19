b546ef4d1241c532bcca1a7ab4f0f80404d15634
Farrel Ardian <farrelardian123@gmail.com>
2023-06-20 00:43:10 +0700

    fix m_barang

--- application/models/M_barang.php
+++ application/models/M_barang.php
@@ -7,7 +7,7 @@ class M_barang extends CI_Model {
     public function get_allData(){
         $this->db->select('*');
         $this->db->from('tb_barang');
-        $this->db->join('tb_kategori', 'tb_kategori.id_kategori = tb_barang.id_barang', 'left');
+        $this->db->join('tb_kategori', 'tb_kategori.id_kategori = tb_barang.id_kategori', 'left');
         $this->db->order_by('id_barang', 'asce');
         return $this->db->get()->result();
     }
