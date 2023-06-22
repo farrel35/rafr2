ede9492e588234106c40ee549daa470c21268cb9
Farrel Ardian <farrelardian123@gmail.com>
2023-06-22 18:41:51 +0700

    fix v_kategori

--- application/views/v_kategori_barang.php
+++ application/views/v_kategori_barang.php
@@ -28,46 +28,49 @@
         <span class="sr-only">Next</span>
     </a>
 </div>
-
-<div class="card card-solid">
-    <div class="card-body pb-0">
-        <div class="row">
-            <?php foreach ($barang as $key => $value) { ?>
-                <div class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch flex-column">
-                    <div class="card bg-light d-flex flex-fill">
-                        <div class="card-header text-muted border-bottom-0">
-                            <h2 class="lead"><b><?= $value->nama_barang ?></b></h2>
-                            <p class="text-muted text-sm">Kategori: <?= $value->nama_kategori ?></p>
-                        </div>
-                        <div class="card-body pt-0">
-                            <div class="row">
-                                <div class="col-12 text-center">
-                                    <img src="<?= base_url('assets/image/' . $value->image) ?>" class="img-fluid" width="250px">
+<div class="row">
+    <div class="col-lg-12">
+        <div class="card card-solid">
+            <div class="card-body pb-0">
+                <div class="row">
+                    <?php foreach ($barang as $key => $value) { ?>
+                        <div class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch flex-column">
+                            <div class="card bg-light d-flex flex-fill">
+                                <div class="card-header text-muted border-bottom-0">
+                                    <h2 class="lead"><b><?= $value->nama_barang ?></b></h2>
+                                    <p class="text-muted text-sm">Kategori: <?= $value->nama_kategori ?></p>
+                                </div>
+                                <div class="card-body pt-0">
+                                    <div class="row">
+                                        <div class="col-12 text-center">
+                                            <img src="<?= base_url('assets/image/' . $value->image) ?>" class="img-fluid" width="250px">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card-footer">
+                                    <div class="row">
+                                        <div class="col-sm-6">
+                                            <div class="text-left">
+                                                <h4><span class="badge bg-primary">Rp <?= number_format($value->harga, 0, ",", ".") ?></span></h4>
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-6">
+                                            <div class="text-right">
+                                                <a href="#" class="btn btn-sm btn-success">
+                                                    <i class="fas fa-eye"></i>
+                                                </a>
+                                                <a href="#" class="btn btn-sm btn-primary">
+                                                    <i class="fas fa-cart-plus"></i>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="card-footer">
-                            <div class="row">
-                                <div class="col-sm-6">
-                                    <div class="text-left">
-                                        <h4><span class="badge bg-primary">Rp <?= number_format($value->harga, 0, ",", ".") ?></span></h4>
-                                    </div>
-                                </div>
-                                <div class="col-sm-6">
-                                    <div class="text-right">
-                                        <a href="#" class="btn btn-sm btn-success">
-                                            <i class="fas fa-eye"></i>
-                                        </a>
-                                        <a href="#" class="btn btn-sm btn-primary">
-                                            <i class="fas fa-cart-plus"></i>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <?php } ?>
                 </div>
-            <?php } ?>
+            </div>
         </div>
     </div>
 </div>
